364b8ca1000acb76dc3545de1959933286f357e5
Vanessa Dias <vds.vanessa94@gmail.com>
2020-07-09 09:16:05 -0300

    Adicionando Element, InicialPage, InicialMap, enums e alterando classes já existentes para usar a nova estrutura

--- src/test/java/cursoTeste/LojaTeste.java
+++ src/test/java/cursoTeste/LojaTeste.java
@@ -22,37 +22,25 @@ public class LojaTeste extends BaseTest {
 	@Test
 	public void realizaPesquisa() {
 		inicialPage.setPesquisa("Fortaleza Digital", Keys.ENTER);
-		WebElement elTituloLivro = Driver.getDriver().findElement(By.xpath("//h2/a"));
-		String livro = elTituloLivro.getText();
+		String livro = inicialPage.getTituloLivro();
 		Assert.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital", livro);
-		WebElement elPreco = Driver.getDriver().findElement(By.cssSelector("#product-price-44"));
-		String preco = elPreco.getText();
+		String preco = inicialPage.getPreco();
 		Assert.assertEquals("R$519,90", preco);
 	}
 	
 	@Test
 	public void realizaPesquisa_assertThat() {
 		inicialPage.setPesquisa("Fortaleza Digital", Keys.ENTER);
-		WebElement elTituloLivro = Driver.getDriver().findElement(By.xpath("//h2/a"));
-		String livro = elTituloLivro.getText();
+		String livro = inicialPage.getTituloLivro();
 		Assert.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital", livro);
-		WebElement elPreco = Driver.getDriver().findElement(By.cssSelector("#product-price-44"));
-		String preco = elPreco.getText();
+		String preco = inicialPage.getPreco();
 		assertThat("R$519,90", is (preco));
 	}
 	
 	@Test
 	public void testClickLista() {
 		inicialPage.setPesquisa("html", Keys.ENTER);
-		List<WebElement> elLivros = Driver.getDriver().findElements(By.cssSelector("ul.products-grid > li"));
-		for (WebElement elLivro : elLivros) {
-			WebElement elTituloLivro = elLivro.findElement(By.cssSelector("h2 > a"));
-			String tituloLivro = elTituloLivro.getText();
-			if(tituloLivro.contains("Ajax com Java")) {
-				WebElement elpreco = elLivro.findElement(By.cssSelector("span.price"));
-				assertThat("R$444,50", is(elpreco.getText()));
-				break;
-			}
-		}
+		String preco = inicialPage.getPrecoLista();
+		assertThat("R$444,50", is(preco));
 	}
 }
